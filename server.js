3dc7b8d3885abe74f9f8fc205b17dc9cbc769ffe
manishraju1996 <manishraju1996@gmail.com>
2017-08-07 02:18:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,8 +57,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
-  var articleName = req.params.articleName
-  res.send(createTemplate(articles[articalName]))
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articalName]));
     });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
