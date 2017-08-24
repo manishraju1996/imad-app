5eb523fbe57d0f912c6d57515f73706cd9e4c736
manishraju1996 <manishraju1996@gmail.com>
2017-08-24 23:15:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var img = document.getElementById('image');
 
  var marginLeft = 0;
  function moveRight(){
-     marginLeft = marginLeft + 10;
+     marginLeft = marginLeft + 1;
       img.style.marginLeft = marginLeft + 'px';
  }
  img.onclick = function() {
