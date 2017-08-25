af6c8131a10f3c85ec6d81bdb3123855dfd22859
manishraju1996 <manishraju1996@gmail.com>
2017-08-25 23:41:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ var htmlTemplate= `
 <html>
     <head>
         <title>${title}</title>
+         <link rel="shortcut icon" href="http://filecatalyst.com/wp-content/uploads/2014/10/direct_160.png" data-reactid=".35kd9lse80.0.0">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link href="/ui/style.css" rel="stylesheet" />
     </head>
